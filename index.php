04608f5b3e50054d1e6e9feee28bb25141623bab
Necha <54299732+necha28@users.noreply.github.com>
2020-03-30 18:32:58 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,45 +1,24 @@
-<?php
-include 'aset/header.php';
-?>
-
-<div class="container">
-	<div class="row mt-4">
-		<div class="col-md">
-		<h2><i class="fas fa-chart-line mr-2"></i>Dashboard</h2>
-		<hr class="bg-light">
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-4">
-			<div class="card bg-danger" style="width: 18rem;">
-  				<div class="card-body text-white">
-    				<h5 class="card-title">Jumlah Buku</h5>
-    				<p class="card-text" style="font-size:60px">350</p>
-   	 				<a href="http://localhost/siperpus/buku/index.php" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
-  				</div>
-			</div>
-		</div>
-		<div class="col-md-4">
-			<div class="card bg-warning" style="width: 18rem;">
-  				<div class="card-body text-white">
-    				<h5 class="card-title">Jumlah Anggota</h5>
-    				<p class="card-text" style="font-size:60px">614</p>
-   	 				<a href="http://localhost/siperpus/anggota/index.php" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
-  				</div>
-			</div>
-		</div>
-		<div class="col-md-4">
-			<div class="card bg-info" style="width: 18rem;">
-  				<div class="card-body text-white">
-    				<h5 class="card-title">Jumlah Transaksi</h5>
-    				<p class="card-text" style="font-size:60px">279</p>
-   	 				<a href="http://localhost/siperpus/transaksi/index.php" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
-  				</div>
-			</div>
-		</div>
-	</div>
-</div>
-
-<?php
-include 'aset/footer.php';
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LogIn</title>
+</head>
+<body>
+    <form action="proses-login.php" method="post">
+        <table>
+            <tr>
+                <td><label for="">Username</label></td>
+                <td><input type="text" name="username" id=""></td>
+            </tr>
+            <tr>
+                <td><label for="">Password</label></td>
+                <td><input type="password" name="password" id=""></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="login" name="btnlogin"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
